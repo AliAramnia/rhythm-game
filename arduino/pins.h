0784493d0293ff3a2b4450451140b829437c738c
Ali Aramnia <aliaramniaa@gmail.com>
2026-08-16 14:34:27 +0200

    feat: define hardware pin assignments

--- arduino/pins.h
+++ arduino/pins.h
@@ -1,7 +1,26 @@
 #ifndef PINS_H
 #define PINS_H
 
-// Hardware pin definitions for the Rhythm Game.
-// Pin assignments will be added exactly as defined in the project.
+// FPGA SPI chip-select pin
+#define FPGA_CS 53
+
+// Game LED pins
+#define LED0 3
+#define LED1 4
+#define LED2 5
+#define LED3 6
+
+// Joystick input pins
+#define JOY_LEFT 14
+#define JOY_UP 2
+#define JOY_RIGHT 16
+#define JOY_DOWN 15
+
+// DAC7611P interface pins
+#define DAC_CS 21
+#define DAC_CLK 20
+#define DAC_SDI 19
+#define DAC_LD 18
+#define DAC_CLR 17
 
 #endif
