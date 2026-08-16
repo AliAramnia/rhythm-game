8b3632f59c57e48a62a307f213c07ba2f7b038a2
Ali Aramnia <aliaramniaa@gmail.com>
2026-08-16 15:28:56 +0200

    fix: complete game round initialization

--- arduino/RhythmGame.ino
+++ arduino/RhythmGame.ino
@@ -11,24 +11,36 @@
 #include "game_state.h"
 
 
-
 // Start a new 10-attempt game round.
 void startGame(void)
 {
+    noInterrupts();
+
     lcgValue = generateSeed();
     lcgIndex = 0;
-
+    currentDirection = 0;
+    movementDetected = false;
+    gameActive = true;
+    gameFinished = false;
+    newValue = false;
     num_correct = 0;
     num_wrong = 0;
 
-    movementDetected = false;
-    newValue = false;
+    interrupts();
 
-    gameActive = true;
-    gameFinished = false;
+    turnOffLeds();
+
+    Serial.println();
+    Serial.println("================================");
+    Serial.println("         NEW GAME STARTED");
+    Serial.println("================================");
+
+    Serial.print("New seed = ");
+    Serial.println(lcgValue);
 
     setupTimer1();
 }
+
 void setup()
 {
 
