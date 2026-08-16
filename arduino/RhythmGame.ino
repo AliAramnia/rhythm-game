9a9ed3dda555bb6ebc362cae5be271e6c89e24af
Ali Aramnia <aliaramniaa@gmail.com>
2026-08-16 15:25:17 +0200

    feat: add game round initialization

--- arduino/RhythmGame.ino
+++ arduino/RhythmGame.ino
@@ -1,3 +1,5 @@
+// Rhythm Game - Arduino Mega
+// Main application file.
 #include "eeprom_manager.h"
 #include "lcg.h"
 #include "adc.h"
@@ -7,15 +9,35 @@
 #include "leds.h"
 #include "joystick.h"
 #include "game_state.h"
-// Rhythm Game - Arduino Mega
-// Main application file.
 
+
+
+// Start a new 10-attempt game round.
+void startGame(void)
+{
+    lcgValue = generateSeed();
+    lcgIndex = 0;
+
+    num_correct = 0;
+    num_wrong = 0;
+
+    movementDetected = false;
+    newValue = false;
+
+    gameActive = true;
+    gameFinished = false;
+
+    setupTimer1();
+}
 void setup()
 {
-    // Hardware initialization will be added here.
+
+
+   // Hardware initialization will be added here.
 }
 
 void loop()
 {
-    // Main game logic will be added here.
+   
+   // Main game logic will be added here.
 }
